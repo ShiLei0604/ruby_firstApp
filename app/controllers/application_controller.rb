037b23825f8b403f86d42e4f9282d4ac1d42c2b0
shi lei <shilei@asukalab.co.jp>
2018-12-19 14:15:56 +0900

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::Base
+    # def hello
+    #     render html:"hello world"
+    # end
+    def sample
+        render html:"sample page"
+    end
+end
